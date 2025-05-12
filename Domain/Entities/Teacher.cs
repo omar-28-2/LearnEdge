e0d5177c307e835fe86a32c587483522dc420b2d
omar-28-2 <o.abdelrahman2228@nu.edu.eg>
2025-05-12 18:40:12 +0300

    edit on teacher entity

--- Domain/Entities/Teacher.cs
+++ Domain/Entities/Teacher.cs
@@ -1,14 +1,12 @@
 ﻿namespace Domain.Entities
 {
-public class Teacher : BaseEntity
-{
-    public string Specialization { get; set; } = null!;
+    public class Teacher : BaseEntity
+    {
+        public string Specialization { get; set; } = null!;
 
-    public Guid UserId { get; set; }
-    public User User { get; set; } = null!;
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
 
-    public ICollection<Course> Courses { get; set; } = new List<Course>();
-}
-
-
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+    }
 }
